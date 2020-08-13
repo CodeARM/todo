31ae077422923f5a1d152c4eb5925236aa62197f
anita <codearm@github.com>
2020-08-13 09:33:02 -0400

    various changes

---
+++ js/javascripts.js
@@ -0,0 +1,45 @@
+document.getElementById("most-important").style.background = 'white'
+
+can the logic be: 
+if a project's area has
+ athelete, then color red for project
+elseif connected
+else if x 
+
+
+document.getElementsByClassName("most-important").style.background = 'white'
+
+
+<script>
+$(document).ready(function(){
+
+// jQuery methods go here...
+
+  document.getElementsByClassName("aof").innerHTML = greeting;
+});
+</script>
+
+.athlete {
+    background-color: rgba(229, 0, 0, 0.7)
+}
+.polished-mirror {
+    background-color: rgba(229, 103, 0, 0.7)
+}
+.connection-and-leadership {
+    background-color: rgba(250, 237, 50, 0.7)
+}
+.expand-professionally {
+    background-color: rgba(172, 205, 3, 0.7)
+}
+.security-and-investment {
+    background-color: rgba(55, 156, 0, 0.7)
+}
+.work-projects {
+    background-color: rgba(0, 228, 224, 0.7)
+}
+.technologist {
+    background-color: rgba(0, 31, 228, 0.7)
+}
+.luxury-and-adventure {
+    background-color: rgba(182, 1, 228, 0.7)
+}
