6bf87cb7f0e5bdb1a0fc654845ab082384813b8e
anita <codearm@github.com>
2020-08-23 09:42:06 -0400

    auth

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('register', views.register, name='register'),
     path('', views.index, name="list"),
     path('where/', views.where),
     path('when/', views.when),
--- tasks/views.py
+++ tasks/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 
 from .models import *
 from .forms import *
 
 # Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']   
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+    
+    context = {'form':form}
+    return render(request, 'registration/register.html', context)
+
 
 def index(request):
     tasks = Task.objects.all()
